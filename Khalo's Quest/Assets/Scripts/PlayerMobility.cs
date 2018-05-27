80893724a9778318d28508f0fa88f289c6cb3982
Andrew Rocchi <andrewroc30@gmail.com>
2018-05-27 18:19:14 -0400

    MovementAndTurning
    Player now moves with WASD and is always facing the mouse

---
+++ Khalo's Quest/Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMobility : MonoBehaviour {
+
+    public float speed;
+
+    void Update()
+    {
+        pointTowardsMouse();
+        move();
+    }
+
+    void pointTowardsMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = Camera.main.nearClipPlane;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        transform.right = direction;
+    }
+
+    void move()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        transform.Translate(xAxis * speed, yAxis * speed, 0, Space.World);
+    }
+}
