7c1deeaf43989d39341088c97a3f9c13c334ea54
ddumplings <mohitp3@gmail.com>
2018-05-27 01:14:07 -0400

    character interface and abstract class

---
+++ Khalo's Quest/Assets/Scripts/Character/ACharacter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ACharacter : ICharacter
+{
+    public abstract void attack();
+    public abstract Vector2 getPosition();
+    public abstract void move();
+
+    int health;
+    int energy;
+    int speed;
+    int stamina;
+    int knowledge;
+    int durability;
+}
---
+++ Khalo's Quest/Assets/Scripts/Character/ICharacter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICharacter {
+
+    // move the Character
+    void move();
+
+    // return the position of the Character
+    Vector2 getPosition();
+
+    // character attacks
+    void attack(); 
+}
