442a63670cc1159406a0e407bae58e0384aa7365
Andrew Rocchi <andrewroc30@gmail.com>
2018-07-06 11:52:50 -0400

    Shooting
    Added shooting by pressing the Fire1 key, which is left click

--- Khalo's Quest/Assets/Scripts/PlayerMobility.cs
+++ Khalo's Quest/Assets/Scripts/PlayerMobility.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerMobility : MonoBehaviour {
 
     public float speed;
+    public GameObject projectilePrefab;
+    private List<GameObject> projectiles = new List<GameObject>();
+    private float projectileVelocity = 100;
 
     void Update()
     {
         pointTowardsMouse();
         move();
+        shoot();
     }
 
     void pointTowardsMouse()
@@ -17,7 +21,7 @@ public class PlayerMobility : MonoBehaviour {
         mousePosition.z = Camera.main.nearClipPlane;
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(mousePosition);
         Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
-        transform.right = direction;
+        transform.up = direction;
     }
 
     void move()
@@ -25,5 +29,36 @@ public class PlayerMobility : MonoBehaviour {
         float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
         transform.Translate(xAxis * speed, yAxis * speed, 0, Space.World);
+    }
+
+    void shoot()
+    {
+        if (Input.GetButtonDown("Fire1")) //&& Time.time > timeUntilFire)
+        {
+            //timeUntilFire = Time.time + 1 / fireRate;
+            GameObject bullet = (GameObject)Instantiate(projectilePrefab, transform.position, transform.rotation);
+            /*Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = Camera.main.nearClipPlane;
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(mousePosition);
+            GameObject bullet = (GameObject)Instantiate(projectilePrefab, transform.position, new Quaternion(mousePos.x, mousePos.y, transform.rotation.z, Quaternion.identity.w));
+            */
+            projectiles.Add(bullet);
+        }
+
+        for (int i = 0; i < projectiles.Count; i++)
+        {
+            GameObject goBullet = projectiles[i];
+            if (goBullet != null)
+            {
+                goBullet.transform.Translate(new Vector3(0, 1) * Time.deltaTime * projectileVelocity);
+                Vector3 bulletScreenPosn = Camera.main.WorldToScreenPoint(goBullet.transform.position);
+                if (bulletScreenPosn.y >= Screen.height || bulletScreenPosn.x >= Screen.width
+                    || bulletScreenPosn.y <= 0 || bulletScreenPosn.x <= 0)
+                {
+                    DestroyObject(goBullet);
+                    projectiles.Remove(goBullet);
+                }
+            }
+        }
     }
 }
