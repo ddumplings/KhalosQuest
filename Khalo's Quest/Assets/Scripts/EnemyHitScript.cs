935ad73b652c90b832742e9a53120aa2e7dab9f4
Andrew Rocchi <andrewroc30@gmail.com>
2018-07-21 19:08:55 -0400

    Enemy Killing
    Made enemy prefab, added killing them on collision with bullet

---
+++ Khalo's Quest/Assets/Scripts/EnemyHitScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Handles getting hit by a bullet
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.tag.Equals("Bullet"))
+        {
+            Destroy(c.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
